4614c09e49d7a8da37655417441f8805d1f6c7b0
Alexey Filimonov <filimonov.alks@gmail.com>
2023-12-09 19:03:26 +0300

    Initial commit

---
+++ cmake/Testing.cmake
@@ -0,0 +1,31 @@
+include_guard(GLOBAL)
+
+enable_testing()
+
+include(FetchContent)
+
+FetchContent_Declare(
+        googletest
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG v1.14.0
+)
+
+# For Windows: Prevent overriding the parent project's
+# compiler/linker settings
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
+option(INSTALL_GMOCK "Install GMock" OFF)
+option(INSTALL_GTEST "Install GTest" OFF)
+
+FetchContent_MakeAvailable(googletest)
+
+include(GoogleTest)
+#include(Coverage)
+include(Memcheck)
+
+macro(AddTests target)
+    target_link_libraries(${target} PRIVATE gtest_main gmock)
+    gtest_discover_tests(${target})
+#    AddCoverage(${target})
+    AddMemcheck(${target})
+endmacro()

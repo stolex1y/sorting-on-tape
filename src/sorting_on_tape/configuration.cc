d8396768d1bba6efdf0e6287a0a430c5285e39c6
Alexey Filimonov <filimonov.alks@gmail.com>
2023-12-09 19:38:44 +0300

    Add configuration

---
+++ src/sorting_on_tape/configuration.cc
@@ -0,0 +1,39 @@
+#include "configuration.h"
+
+#include <fstream>
+#include <limits>
+
+namespace sot {
+
+Configuration::Configuration(const std::string &file_name) {
+  ReadParamsFromFile(file_name);
+}
+
+std::int64_t Configuration::GetProperty(const std::string &key, const std::int64_t default_value)
+    const {
+  const auto value = params.find(key);
+  if (value != params.end()) {
+    return value->second;
+  }
+  return default_value;
+}
+
+void Configuration::ReadParamsFromFile(const std::string &file_name) {
+  std::ifstream input(file_name);
+  if (!input) {
+    return;
+  }
+  std::string key;
+  while (getline(input, key, '=')) {
+    int64_t value;
+    input >> value;
+    if (input.fail()) {
+      input.clear();
+    } else {
+      params.emplace(key, value);
+    }
+    input.ignore(std::numeric_limits<std::streamsize>::max(), input.widen('\n'));
+  }
+}
+
+}  // namespace sot
---
+++ src/sorting_on_tape/configuration.h
@@ -0,0 +1,40 @@
+#ifndef CONFIGURATION_H
+#define CONFIGURATION_H
+
+#include <cstdint>
+#include <string>
+#include <unordered_map>
+
+namespace sot {
+
+/**
+ * \brief Класс, выполняющий чтение конфигурации из файла.
+ */
+class Configuration {
+ public:
+  /// Имя файла с конфигурацией по умолчанию.
+  static constexpr auto kDefaultFileName = "config.properties";
+
+  explicit Configuration(const std::string &file_name = kDefaultFileName);
+  Configuration(Configuration &other) = delete;
+  Configuration &operator=(Configuration &other) = delete;
+  virtual ~Configuration() = default;
+
+  /**
+   * \brief Получить значение свойства по ключу либо переданное значение по умолчанию.
+   */
+  std::int64_t GetProperty(const std::string &key, std::int64_t default_value) const;
+
+ protected:
+  std::unordered_map<std::string, std::int64_t> params;
+
+ private:
+  /**
+   * \brief Прочитать значения параметров из файла.
+   */
+  void ReadParamsFromFile(const std::string &file_name);
+};
+
+}  // namespace sot
+
+#endif  // CONFIGURATION_H

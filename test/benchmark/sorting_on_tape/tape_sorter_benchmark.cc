560a242aefb508655215eed1368d8825321de082
Alexey Filimonov <filimonov.alks@gmail.com>
2023-12-14 02:27:14 +0300

    Update benchmarks with new durations

--- test/benchmark/sorting_on_tape/tape_sorter_benchmark.cc
+++ test/benchmark/sorting_on_tape/tape_sorter_benchmark.cc
@@ -10,8 +10,8 @@ class TapeSorterBenchmark : public TapeSorterTestBase<Value>, public ::benchmark
  public:
   void SetUp(::benchmark::State &st) override;
   void TearDown(::benchmark::State &st) override;
-  void SetUpTapeDurations(std::chrono::milliseconds base_duration = 1ms);
-  void SetUpMemoryLimit(std::size_t number_count, uint64_t divider = 8);
+  void SetUpTapeDurations(std::chrono::microseconds base_duration = 5us);
+  void SetUpMemoryLimit(std::size_t value_count, uint64_t divider = 10);
 };
 
 void TapeSorterBenchmark::SetUp(::benchmark::State &st) {
@@ -27,74 +27,83 @@ void TapeSorterBenchmark::TearDown(::benchmark::State &st) {
   Fixture::TearDown(st);
 }
 
-void TapeSorterBenchmark::SetUpTapeDurations(const std::chrono::milliseconds base_duration) {
-  config_.SetMoveDuration(base_duration / 2);
+void TapeSorterBenchmark::SetUpTapeDurations(const std::chrono::microseconds base_duration) {
+  config_.SetMoveDuration(base_duration / 5);
   config_.SetReadDuration(base_duration);
   config_.SetWriteDuration(base_duration);
-  config_.SetRewindDuration(base_duration * 4);
+  config_.SetRewindDuration(base_duration * 10);
 }
 
-void TapeSorterBenchmark::SetUpMemoryLimit(const std::size_t number_count, const uint64_t divider) {
-  config_.SetMemoryLimit(number_count * sizeof(Value) / divider);
+void TapeSorterBenchmark::SetUpMemoryLimit(const std::size_t value_count, const uint64_t divider) {
+  config_.SetMemoryLimit(value_count * sizeof(Value) / divider);
 }
 
 BENCHMARK_DEFINE_F(TapeSorterBenchmark, SortRandomArray)(::benchmark::State &state) {
-  const auto number_count = state.range(0);
-  SetUpMemoryLimit(number_count);
-  const auto numbers = InitInputDataWithRandomNumbers(number_count);
+  const auto value_count = state.range(0);
+  SetUpMemoryLimit(value_count);
+  const auto values = InitInputDataWithRandomValues(value_count);
   for (auto _ : state) {
-    const auto sorted_numbers = SortTape();
+    const auto sorted_values = SortTape();
   }
 }
-BENCHMARK_REGISTER_F(TapeSorterBenchmark, SortRandomArray)->Arg(40)->Arg(400)->Arg(4000);
+BENCHMARK_REGISTER_F(TapeSorterBenchmark, SortRandomArray)->Arg(5000)->Arg(10000)->Arg(25000);
 
 BENCHMARK_DEFINE_F(TapeSorterBenchmark, SortSortedArray)(::benchmark::State &state) {
-  const size_t number_count = state.range(0);
-  SetUpMemoryLimit(number_count);
-  auto expected_numbers = GenerateRandomArray(number_count);
-  std::ranges::sort(expected_numbers);
-  CreateFileWithBinaryContent(input_file_path_, expected_numbers);
+  const size_t value_count = state.range(0);
+  SetUpMemoryLimit(value_count);
+  auto expected_values = GenerateRandomArray<Value>(value_count);
+  std::ranges::sort(expected_values);
+  CreateFileWithBinaryContent(input_file_path_, expected_values);
   for (auto _ : state) {
-    const auto sorted_numbers = SortTape();
+    const auto sorted_values = SortTape();
   }
 }
-BENCHMARK_REGISTER_F(TapeSorterBenchmark, SortSortedArray)->Arg(40)->Arg(400)->Arg(4000);
+BENCHMARK_REGISTER_F(TapeSorterBenchmark, SortSortedArray)->Arg(5000)->Arg(10000)->Arg(25000);
 
 BENCHMARK_DEFINE_F(TapeSorterBenchmark, SortSortedInReverseOrderArray)(::benchmark::State &state) {
-  const size_t number_count = state.range(0);
-  SetUpMemoryLimit(number_count);
-  auto expected_numbers = GenerateRandomArray(number_count);
-  std::ranges::sort(expected_numbers, std::greater());
-  CreateFileWithBinaryContent(input_file_path_, expected_numbers);
+  const size_t value_count = state.range(0);
+  SetUpMemoryLimit(value_count);
+  auto expected_values = GenerateRandomArray<Value>(value_count);
+  std::ranges::sort(expected_values, std::greater());
+  CreateFileWithBinaryContent(input_file_path_, expected_values);
   for (auto _ : state) {
-    const auto sorted_numbers = SortTape();
+    const auto sorted_values = SortTape();
   }
 }
 BENCHMARK_REGISTER_F(TapeSorterBenchmark, SortSortedInReverseOrderArray)
-    ->Arg(40)
-    ->Arg(400)
-    ->Arg(4000);
+    ->Arg(5000)
+    ->Arg(10000)
+    ->Arg(25000);
 
 BENCHMARK_DEFINE_F(TapeSorterBenchmark, SortArrayWithMemoryLimit)(::benchmark::State &state) {
-  constexpr size_t number_count = 1000;
-  SetUpMemoryLimit(number_count, state.range(0));
-  const auto numbers = InitInputDataWithRandomNumbers(1000);
+  constexpr size_t value_count = 20000;
+  SetUpMemoryLimit(value_count, state.range(0));
+  const auto values = InitInputDataWithRandomValues(value_count);
   for (auto _ : state) {
-    const auto sorted_numbers = SortTape();
+    const auto sorted_values = SortTape();
   }
 }
-BENCHMARK_REGISTER_F(TapeSorterBenchmark, SortArrayWithMemoryLimit)->Arg(8)->Arg(4)->Arg(2)->Arg(1);
+BENCHMARK_REGISTER_F(TapeSorterBenchmark, SortArrayWithMemoryLimit)
+    ->Arg(1)
+    ->Arg(5)
+    ->Arg(20)
+    ->Arg(100)
+    ->Arg(1000);
 
 BENCHMARK_DEFINE_F(TapeSorterBenchmark, SortArrayWithDifferentDelays)(::benchmark::State &state) {
-  constexpr size_t number_count = 500;
-  SetUpMemoryLimit(number_count);
-  SetUpTapeDurations(state.range(0) * 1ms);
-  const auto numbers = InitInputDataWithRandomNumbers(number_count);
+  constexpr size_t value_count = 20000;
+  SetUpMemoryLimit(value_count);
+  SetUpTapeDurations(state.range(0) * 1us);
+  const auto values = InitInputDataWithRandomValues(value_count);
   for (auto _ : state) {
-    const auto sorted_numbers = SortTape();
+    const auto sorted_values = SortTape();
   }
 }
-BENCHMARK_REGISTER_F(TapeSorterBenchmark, SortArrayWithDifferentDelays)->Arg(1)->Arg(4)->Arg(16);
+BENCHMARK_REGISTER_F(TapeSorterBenchmark, SortArrayWithDifferentDelays)
+    ->Arg(1)
+    ->Arg(10)
+    ->Arg(20)
+    ->Arg(100);
 
 BENCHMARK_MAIN();
 

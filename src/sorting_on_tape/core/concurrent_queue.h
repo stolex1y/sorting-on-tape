a15c65c9f6d6f00ce282803ba7d1cad81beeb751
Alexey Filimonov <filimonov.alks@gmail.com>
2023-12-21 17:31:30 +0300

    Add blocking pop

--- src/sorting_on_tape/core/concurrent_queue.h
+++ src/sorting_on_tape/core/concurrent_queue.h
@@ -1,6 +1,7 @@
 #ifndef CONCURRENT_QUEUE_H
 #define CONCURRENT_QUEUE_H
 
+#include <condition_variable>
 #include <mutex>
 #include <optional>
 #include <queue>
@@ -14,27 +15,41 @@ class ConcurrentQueue {
   virtual ~ConcurrentQueue() = default;
 
   void Push(T new_value);
-  std::optional<T> Pop();
+  std::optional<T> TryPop();
+  T Pop();
   bool Empty() const;
   size_t Size() const;
 
  protected:
   mutable std::shared_mutex mutex_;
   std::queue<T> queue_;
+  std::condition_variable_any has_elements_;
 };
 
 template <typename T>
 void ConcurrentQueue<T>::Push(T new_value) {
   std::lock_guard lock(mutex_);
   queue_.push(std::move(new_value));
+  has_elements_.notify_one();
 }
 
 template <typename T>
-std::optional<T> ConcurrentQueue<T>::Pop() {
+std::optional<T> ConcurrentQueue<T>::TryPop() {
   std::lock_guard lock(mutex_);
   if (queue_.empty()) {
     return std::nullopt;
   }
+  T value(std::move(queue_.front()));
+  queue_.pop();
+  return value;
+}
+
+template <typename T>
+T ConcurrentQueue<T>::Pop() {
+  std::unique_lock lock(mutex_);
+  has_elements_.wait(lock, [this] {
+    return !queue_.empty();
+  });
   T value(std::move(queue_.front()));
   queue_.pop();
   return value;
